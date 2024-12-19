dc94832f17c20e3bb8d8a4e700fec1c629ee5b1e
Anatolii Zorin <Zorger27@GMail.com>
2024-12-20 01:13:56 +0200

    Small changes

--- src/store/modules/service/infoStore.ts
+++ src/store/modules/service/infoStore.ts
@@ -3,87 +3,123 @@ const state = {
     [
       {
         id: 1,
-        title: "Three.js",
-        version: "169",
-        url: "https://github.com/mrdoob/three.js/releases"
-      },
-      {
-        id: 2,
         title: "Vue.js",
-        version: "3.4.21",
+        version: "3.5.13",
         url: "https://v3.ru.vuejs.org"
       },
       {
-        id: 3,
+        id: 2,
         title: "Vue Router",
-        version: "4.3.0",
+        version: "4.5.0",
         url: "https://router.vuejs.org"
       },
       {
-        id: 4,
+        id: 3,
         title: "VueX",
         version: "4.1.0",
         url: "https://vuex.vuejs.org"
       },
       {
+        id: 4,
+        title: "Webpack",
+        version: "5.97.1",
+        url: "https://webpack.js.org"
+      },
+      {
         id: 5,
-        title: "Vue i18n",
-        version: "9.8.0",
+        title: "Vue I18n",
+        version: "9.9.1",
         url: "https://vue-i18n.intlify.dev"
       },
       {
         id: 6,
+        title: "Three.js",
+        version: "171",
+        url: "https://github.com/mrdoob/three.js/releases"
+      },
+      {
+        id: 7,
+        title: "Axios",
+        version: "1.7.9",
+        url: "https://github.com/axios/axios"
+      },
+      {
+        id: 8,
         title: "HTML5",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/HTML/Introduction_to_HTML"
       },
       {
-        id: 7,
+        id: 9,
         title: "CSS3",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/CSS/First_steps"
       },
       {
-        id: 8,
+        id: 10,
         title: "FlexBox CSS",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/CSS/CSS_layout/Flexbox"
       },
       {
-        id: 9,
+        id: 11,
         title: "JavaScript",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/JavaScript"
       },
       {
-        id: 10,
+        id: 12,
         title: "TypeScript",
-        version: "4.5.5",
+        version: "5.7.2",
         url: "https://www.typescriptlang.org/"
       },
       {
-        id: 11,
-        title: "Axios",
-        version: "1.7.7",
-        url: "https://github.com/axios/axios"
-      },
-      {
-        id: 12,
-        title: "Google Search Console",
-        version: "",
-        url: "https://search.google.com/search-console"
-      },
-      {
         id: 13,
-        title: "Google Analytics",
-        version: "4.0",
-        url: "https://analytics.google.com"
+        title: "Markdown",
+        version: "",
+        url: "https://www.markdownguide.org"
       },
       {
         id: 14,
         title: "OpenWeather Map API",
         version: "",
         url: "https://openweathermap.org"
+      },
+      {
+        id: 15,
+        title: "Google Search Console",
+        version: "",
+        url: "https://search.google.com/search-console"
+      },
+      {
+        id: 16,
+        title: "Google Analytics",
+        version: "4.0",
+        url: "https://analytics.google.com"
+      },
+      {
+        id: 17,
+        title: "Open Graph protocol",
+        version: "",
+        url: "https://ogp.me"
+      },
+      {
+        id: 18,
+        title: "Favicon",
+        version: "",
+        url: "https://developer.mozilla.org/en-US/docs/Glossary/Favicon"
+      },
+      {
+        id: 19,
+        title: "Sitemap",
+        version: "",
+        url: "https://www.sitemaps.org"
+      },
+      {
+        id: 20,
+        title: "Robots.txt",
+        version: "",
+        url: "https://developers.google.com/search/docs/crawling-indexing/robots/intro"
       },
     ]
 };
