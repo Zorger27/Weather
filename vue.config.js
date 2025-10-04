e693d0ad929cc234e594f98a388e9e5524a751ae
Anatolii Zorin <Zorger27@GMail.com>
2025-10-04 23:13:15 +0300

    Small changes

--- vue.config.js
+++ vue.config.js
@@ -78,11 +78,11 @@ module.exports = {
       new SitemapPlugin({
         base: 'https://weather-zorger.vercel.app', // Базовый URL моего сайта
         paths: [
-          { path: '/', priority: 1, changefreq: 'always' },
-          { path: '/project2', priority: 1, changefreq: 'daily' },
-          { path: '/project3', priority: 1, changefreq: 'daily' },
-          { path: '/project4', priority: 1, changefreq: 'daily' },
-          { path: '/about', priority: 1, changefreq: 'always' },
+          { path: '/', priority: 1.0, changefreq: 'daily' },
+          { path: '/project2', priority: 0.8, changefreq: 'weekly' },
+          { path: '/project3', priority: 0.8, changefreq: 'weekly' },
+          { path: '/project4', priority: 0.8, changefreq: 'weekly' },
+          { path: '/about', priority: 0.6, changefreq: 'monthly' },
         ],
         options: {
           skipgzip: true
